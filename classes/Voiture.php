30d787b4114b794ad591db70eb41f8eba66e8211
Sofien <sofiendu76800@gmail.com>
2020-01-28 11:20:14 +0100

    Création de la classe voiture

--- classes/Voiture.php
+++ classes/Voiture.php
@@ -6,6 +6,22 @@ class Voiture
   public $modele;
   public $couleur;
   public $dimensions = [0, 0, 0];
+  public $masse;
+  public $vitesse = 0;
+
+  public function __construct($mar, $mod, $cou, $dim, $mas)
+  {
+    $this->marque = $mar;
+    $this->modele = $mod;
+    $this->couleur = $cou;
+    $this->dimensions = $dim;
+    $this->masse = $mas;
+  }
+
+  public function afficherMessage()
+  {
+    echo "Je suis Michel";
+  }
 }
 
 
